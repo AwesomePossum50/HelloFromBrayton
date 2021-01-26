9d2dd8db5de7938e905e6fb7ef6ac5022de98de7
AwesomePossum50 <12309261+AwesomePossum50@users.noreply.github.com>
2021-01-26 11:32:45 -0800

    Initial commit

---
+++ src/rogers/Main.java
@@ -0,0 +1,9 @@
+package rogers;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String yourName = "Brayton";
+        System.out.printf("Hello, my name is %s!", yourName);
+    }
+}

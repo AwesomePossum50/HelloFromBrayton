d1438656897071b297a24e77dec16be1d578f5e8
xXShadeMakerXx <xchristopherdittox@comcast.net>
2021-01-26 22:01:09 -0800

    Fixed newline.

--- src/rogers/Main.java
+++ src/rogers/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String yourName = "Brayton";
         String teamMatesName = "Christopher";
-        System.out.printf("Hello, my name is %s", yourName + "!\n");
+        System.out.printf("Hello, my name is %s!%n", yourName);
         System.out.printf("Hello, " + yourName + " my name is %s!", teamMatesName);
     }
 }

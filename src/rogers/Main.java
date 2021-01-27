e19ceea4af5b4cce0f316e2623c215bf47eeba29
xXShadeMakerXx <xchristopherdittox@comcast.net>
2021-01-26 18:41:11 -0800

    Added hello response.

--- src/rogers/Main.java
+++ src/rogers/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         String yourName = "Brayton";
-        System.out.printf("Hello, my name is %s!", yourName);
+        String teamMatesName = "Christopher";
+        System.out.printf("Hello, my name is %s", yourName + "!\n");
+        System.out.printf("Hello, " + yourName + " my name is %s!", teamMatesName);
     }
 }
